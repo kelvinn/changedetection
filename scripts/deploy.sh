416591ff9ab3e715a803a595add63acd2f17dd34
Kelvin Nicholson <kelvin@kelvinism.com>
2023-10-15 09:19:31 +1100

    Make sure just a single machine exists

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,7 @@ APP_NAME="changedetection-a32cf"
 # docker push registry.fly.io/$APP_NAME:deployment-$TAG
 # fly machine update $MACHINE_ID --yes --image registry.fly.io/$APP_NAME:deployment-$TAG --schedule=daily --metadata fly_process_group=app
 fly deploy --wait-timeout 240
+fly scale count 1 --yes --process-group worker
 
 MACHINE_ID=$(fly machine list --json | jq -r -c '.[] | select(.config | .metadata | .fly_process_group | contains("worker")) | .id')
 fly machine update $MACHINE_ID --yes --schedule=daily --metadata fly_process_group=worker
