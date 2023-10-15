fe07ec369a10c6431f74d9dae8239c04664c5a40
Kelvin Nicholson <kelvin@kelvinism.com>
2023-10-15 11:11:03 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,17 @@
+#!/bin/bash
+
+# Abort the script if any command fails
+set -e
+
 
 # See README.md for how to get these values
 
 TAG="0.0.$GITHUB_RUN_NUMBER" # Initially set this to "0.0.1", and then update to come from Github Actions
-# MACHINE_ID=$MACHINE_ID  # Initially hard code this to your machine ID for the first deploy, e.g. "e2865c22a34548", and then update to use whatever env var is in Github Actions
 APP_NAME="changedetection-a32cf"
 
-# docker build -t registry.fly.io/$APP_NAME:deployment-$TAG . --platform linux/amd64
-# docker push registry.fly.io/$APP_NAME:deployment-$TAG
-# fly machine update $MACHINE_ID --yes --image registry.fly.io/$APP_NAME:deployment-$TAG --schedule=daily --metadata fly_process_group=app
-fly deploy --wait-timeout 240
-fly scale count 1 --yes --process-group worker
+fly deploy --ha=false --strategy immediate --wait-timeout 240
 
-MACHINE_ID=$(fly machine list --json | jq -r -c '.[] | select(.config | .metadata | .fly_process_group | contains("worker")) | .id')
-fly machine update $MACHINE_ID --yes --schedule=daily --metadata fly_process_group=worker
+sleep 5 # Wait for machine to get replaced
+
+MACHINE_ID=$(fly machine list --json | jq -r '.[] | select(.config.metadata.fly_process_group == "worker") | .id')
+fly machine update $MACHINE_ID --yes --restart on-fail --skip-health-checks --schedule=daily --metadata fly_process_group=worker
