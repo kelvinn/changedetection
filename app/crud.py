c2ebe38a927143de48556591f9140779a63b1cc7
Kelvin Nicholson <kelvin@kelvinism.com>
2025-10-27 19:03:10 +1100

    Update crud.py

--- app/crud.py
+++ app/crud.py
@@ -10,5 +10,5 @@ def get_product(db: Session, product_gid: int):
     return product
 
 def get_products(db: Session):
-    products = db.query(models.Product).all()
+    products = db.query(models.Product).first()
     return products
